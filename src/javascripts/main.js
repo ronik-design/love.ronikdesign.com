d3c74e7930989ca69f589104363163512fe0cbf1
jaredpike <jaredpike92@gmail.com>
2018-01-02 15:02:11 -0500

    switch to threejs setup

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,42 @@
-require('aframe');
-require('aframe-text-geometry-component');
+const THREE = require('three');
 
-require('./components/update-text');
+let camera, scene, renderer, geometry, material, mesh;
+
+init();
+animate();
+
+function init() {
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+  camera.position.z = 1000;
+
+  scene = new THREE.Scene();
+
+  geometry = new THREE.TorusKnotGeometry(70, 20, 100, 16);
+  material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+  window.addEventListener('resize', onWindowResize, false);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  mesh.rotation.x += 0.02;
+  mesh.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
