2c961eded9ef8e9969e06c7a418d759b66e5a6a3
jaredpike <jaredpike92@gmail.com>
2018-02-12 11:53:24 -0500

    make sure all messages are punny

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -5,14 +5,14 @@ const messages = [
   ['RGB', 'mine?'],
   ['You are the', 'HTML to my', 'CSS'],
   ['URL', 'mine'],
-  ['Spread', 'the', 'love'],
   ['You are', 'off the', 'blockchain'],
+  ['I am', 'all hover', 'you'],
   ['You are', 'just my', 'type'],
-  ['Crazy', '4', 'you'],
   ['You kern', 'me on'],
   ['CSS', 'me'],
   ['You are', 'the lorem', 'to my ipsum'],
   ['I am', 'font of you'],
+  ['You had', 'me at', 'hello world']
 ];
 
 module.exports = messages;
